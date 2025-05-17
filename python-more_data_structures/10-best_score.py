36332590930bc9b5eb8f76dcc2d68472c54ff0ee
maram-ra <maramraddad33@gmail.com>
2025-05-17 15:19:44 +0000

    Completed all tasks for python-more_data_structures project

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    max_key = None
+    max_value = float('-inf')
+
+    for key in a_dictionary:
+        if a_dictionary[key] > max_value:
+            max_value = a_dictionary[key]
+            max_key = key
+
+    return max_key
