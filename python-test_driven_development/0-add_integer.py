4c2ef963205f612609b59612f6b458a322fbe21c
maram-ra <maramraddad33@gmail.com>
2025-05-18 14:39:59 +0000

    Add float overflow and NaN handling to add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,8 @@
 This module provides a function to add two integers.
 """
 
+import math
+
 def add_integer(a, b=98):
     """
     Returns the sum of a and b as integers.
@@ -21,9 +23,23 @@ def add_integer(a, b=98):
     Traceback (most recent call last):
         ...
     TypeError: a must be an integer
+    >>> add_integer(float('inf'), 1)
+    Traceback (most recent call last):
+        ...
+    ValueError: cannot convert float NaN to integer
+    >>> add_integer(float('nan'), 1)
+    Traceback (most recent call last):
+        ...
+    ValueError: cannot convert float NaN to integer
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    if isinstance(a, float) and (math.isnan(a) or math.isinf(a)):
+        raise ValueError("cannot convert float NaN to integer")
+    if isinstance(b, float) and (math.isnan(b) or math.isinf(b)):
+        raise ValueError("cannot convert float NaN to integer")
+
     return int(a) + int(b)
