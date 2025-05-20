ed715db565464ba0ba3c5faae319020b9a3a9056
maram-ra <maramraddad33@gmail.com>
2025-05-20 14:38:59 +0000

    Add Square class with input validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize the square with an optional size."""
-        if not isinstance (size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
