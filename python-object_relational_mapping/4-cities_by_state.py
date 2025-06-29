f7d983b8be68a9d839606ad18e3ad7e3b47e8290
maram-ra <maramraddad33@gmail.com>
2025-06-29 15:58:35 +0000

    fix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,9 +14,7 @@ if __name__ == "__main__":
         port=3306
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS = %s;", (sys.argv[4],))
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
     for state in cursor.fetchall():
         print(state)
